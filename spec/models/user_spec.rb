a799d2e0d4a017151765e8f50f04188fb1a3c452
Tim Allen <tnelsallen3@gmail.com>
2019-05-20 16:32:51 -0600

    Add test it has a name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "exists" do
-    user = User.create(name: "Jim")
-    expect(user).to be_a(User)
+  before :each do
+    @user = User.create(name: 'Jim')
+  end
+
+  it 'exists' do
+    expect(@user).to be_a(User)
+  end
+
+  it 'has attributes' do
+    expect(@user.name).to eq('Jim')
   end
 end
