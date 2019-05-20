5c474b60e4a3ff5db9ce9cfe5ffada4e61fb9d8a
Tim Allen <tnelsallen3@gmail.com>
2019-05-20 17:13:20 -0600

    Add User relationships and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'relationships' do
+    it { should have_many :user_routines }
+    it { should have_many :routines }
+  end
 end
