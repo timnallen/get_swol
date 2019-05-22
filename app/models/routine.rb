06796432374f1822485c500e3067c9b885d13666
Tim Allen <tnelsallen3@gmail.com>
2019-05-22 13:39:29 -0600

    Add relationship for exercises to routine model

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,6 +1,8 @@
 class Routine < ApplicationRecord
   has_many :user_routines
   has_many :users, through: :user_routines
-  
+  has_many :exercise_routines
+  has_many :exercises, through: :exercise_routines
+
   validates_presence_of :name
 end
