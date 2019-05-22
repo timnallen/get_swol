b50d177bc38953344c85a95a79e5639b99ad09c5
Tim Allen <tnelsallen3@gmail.com>
2019-05-22 14:27:59 -0600

    Format routine serializer to return exercises with reps and sets and e_r values

--- app/serializers/routine_serializer.rb
+++ app/serializers/routine_serializer.rb
@@ -1,4 +1,20 @@
 class RoutineSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
+
+  attribute :exercises do |object|
+    object.exercises.map do |ex|
+      er = ExerciseRoutine.find_by(routine_id: object.id, exercise_id: ex.id)
+      {
+        id: ex.id,
+        name: ex.name,
+        category: ex.category,
+        muscle: ex.muscle,
+        reps: er.reps,
+        sets: er.sets,
+        duration: er.duration,
+        weight: er.weight
+      }
+    end
+  end
 end
