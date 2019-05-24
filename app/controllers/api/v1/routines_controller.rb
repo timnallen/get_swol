92c4d042e36e31bb4a5fd48361d38554959403a9
Tim Allen <tnelsallen3@gmail.com>
2019-05-24 11:23:41 -0600

    Add routine controller create action

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -6,4 +6,20 @@ class Api::V1::RoutinesController < ApplicationController
   def show
     render json: RoutineSerializer.new(Routine.find(params[:id]))
   end
+
+  def create
+    user = User.find(params[:user_id]) if params[:user_id]
+    routine = Routine.new(routine_params)
+    if user && routine.save
+      render json: {message: "You have successfully created a workout sequence!"}, status: 201
+    else
+      four_oh_four
+    end
+  end
+
+  private
+
+  def routine_params
+    params.permit(:name)
+  end
 end
