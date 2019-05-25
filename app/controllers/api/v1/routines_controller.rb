edac470a534fb3b50e1c856ff436ad7b9fc6fd20
Tim Allen <tnelsallen3@gmail.com>
2019-05-25 10:41:27 -0600

    Add id to post response for creating a routine

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::RoutinesController < ApplicationController
     user = User.find(params[:user_id]) if params[:user_id]
     routine = Routine.new(routine_params)
     if user && routine.save
-      render json: {message: "You have successfully created a workout sequence!"}, status: 201
+      render json: {message: "You have successfully created a routine!", id: routine.id}, status: 201
     else
       four_oh_four
     end
