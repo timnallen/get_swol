2017fa63ddaddf1ee480c59057ceaec18be8e866
Tim Allen <tnelsallen3@gmail.com>
2019-05-26 10:23:52 -0600

    Add happ and sad path tests for adding an exercise to a routine

--- spec/requests/api/v1/exercise_routine_request_spec.rb
+++ spec/requests/api/v1/exercise_routine_request_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 describe 'Exercise Routines API' do
   describe 'Endpoints' do
-    it 'can get a list of exercise routines' do
+    before :each do
       ex1 = Exercise.create(name: 'Squats', category: 'legs', muscle: 'Quadriceps', equipment_required: 'none')
       ex2 = Exercise.create(name: 'Hammy Lifts', category: 'legs', muscle: 'Hamstrings', equipment_required: 'That machine')
       ex3 = Exercise.create(name: 'Jogging', category: 'legs', muscle: 'Legs', equipment_required: 'none')
-      r1 = Routine.create(name: 'Leg Day')
-      ExerciseRoutine.create(exercise: ex1, routine: r1, reps: 12, sets: 4)
-      ExerciseRoutine.create(exercise: ex2, routine: r1, reps: 10, sets: 3, weight: 25)
-      ExerciseRoutine.create(exercise: ex3, routine: r1, duration: 30)
+      @r1 = Routine.create(name: 'Leg Day')
+      ExerciseRoutine.create(exercise: ex1, routine: @r1, reps: 12, sets: 4)
+      ExerciseRoutine.create(exercise: ex2, routine: @r1, reps: 10, sets: 3, weight: 25)
+      ExerciseRoutine.create(exercise: ex3, routine: @r1, duration: 30)
+    end
 
+    it 'can get a list of exercise routines' do
       get '/api/v1/exercise_routines'
 
       expect(response).to be_successful
@@ -29,6 +31,27 @@ describe 'Exercise Routines API' do
       expect(ers[:data][0][:attributes][:reps]).to eq(12)
       expect(ers[:data][0][:attributes][:sets]).to eq(4)
       expect(ers[:data][1][:attributes][:sets]).to eq(3)
+    end
+
+    it 'can add an exercise to a routine' do
+      ex = Exercise.create(name: 'Scissor Kicks', equipment_required: 'none', muscle: 'legs', category: 'legs')
+      body = {
+        exercise_id: ex.id,
+        routine_id: @r1.id
+      }
+
+      post '/api/v1/exercise_routines', params: body
+
+      expect(response.status).to eq(201)
+      res = JSON.parse(response.body, symbolize_names: true)
+      expect(res.keys).to include(:id)
+      expect(res[:message]).to eq("You have added #{ex.name} to #{@r1.name}!")
+    end
+
+    it 'cant add an exercise to a routine without a body' do
+      post '/api/v1/exercise_routines'
+
+      expect(response.status).to eq(404)
     end
   end
 end
