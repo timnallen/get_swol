b0c3a906b382844e9901e00c398f617832258979
Tim Allen <tnelsallen3@gmail.com>
2019-05-26 10:29:00 -0600

    Add creating an exercise routine functionality

--- app/controllers/api/v1/exercise_routines_controller.rb
+++ app/controllers/api/v1/exercise_routines_controller.rb
@@ -2,4 +2,21 @@ class Api::V1::ExerciseRoutinesController < ApplicationController
   def index
     render json: ExerciseRoutineSerializer.new(ExerciseRoutine.all)
   end
+
+  def create
+    er = ExerciseRoutine.new(exercise_routine_params)
+    if er.save
+      ex = er.exercise
+      rou = er.routine
+      render json: {id: er.id, message: "You have added #{ex.name} to #{rou.name}!"}, status: 201
+    else
+      four_oh_four
+    end
+  end
+
+  private
+
+  def exercise_routine_params
+    params.permit(:exercise_id, :routine_id, :reps, :sets, :duration, :weight)
+  end
 end
