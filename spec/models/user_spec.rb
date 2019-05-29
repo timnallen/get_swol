b3d1641ebd5677e53eedabbd689f80c9a2fd80a9
Tim Allen <tnelsallen3@gmail.com>
2019-05-29 10:48:18 -0600

    Add user validation specs for email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
-    it {should validate_confirmation_of(:password)}
+    it { should validate_confirmation_of :password }
   end
 
   describe 'relationships' do
