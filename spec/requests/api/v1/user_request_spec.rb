eeaf69d55f415be9e7df3decbce7eae579ca61ad
Tim Allen <tnelsallen3@gmail.com>
2019-05-29 11:23:56 -0600

    Add test for user login endpoints, happy and sad

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -37,5 +37,32 @@ describe 'User Routines API' do
 
       expect(response.status).to eq(409)
     end
+
+    it 'allows me to login a user with the correct email and password' do
+      example = User.create(name: 'Tim', email: 'email@email.com', password: 'hjfkhjk', password_confirmation: 'hjfkhjk')
+
+      post '/api/v1/login', params: {
+        email: 'email@email.com',
+        password: 'hjfkhjk'
+      }
+
+      expect(response).to be_successful
+      user = JSON.parse(response.body, symbolize_names: true)
+      expect(user[:data][:id]).to eq(example.id)
+      expect(user[:data][:attributes].keys).to include(:api_key)
+      expect(user[:data][:attributes][:email]).to eq(example.email)
+    end
+
+    it 'does not allow me to login a user without the correct email and password' do
+      User.create(name: 'Tim', email: 'email@email.com', password: 'hjfkhjk', password_confirmation: 'hjfkhjk')
+
+      post '/api/v1/login', params: {
+        email: 'email@email.com'
+      }
+
+      expect(response.status).to eq(401)
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:message]).to eq("Unauthorized. Incorrect email and/or password")
+    end
   end
 end
